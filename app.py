927e10661139af280e4facbd178a81f2b9282fc0
ObviousCloud <weinreichchris@gmail.com>
2018-11-30 12:59:21 -0800

    more data

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import pickle
 app = Flask(__name__)
 
 #load model
-model = pickle.load(open('model.pkl', 'r'))
+model = pickle.load(open('decision_tree_model.pkl', 'r'))
 
 #define route with post request
 @app.route('/', methods=["POST"])
 def index():
 	# get array of features from post requests
-	feature_array = request.get_json()
+	features = request.get_json()
 
 	#creating a response object
     #storing the model's prediction in the object
 	response = {}
-	response['predictions'] = model.predict([feature_array]).tolist()
+	response['predictions'] = model.predict([features]).tolist()
 
 	return jsonify(response)
 
