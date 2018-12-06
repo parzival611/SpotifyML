c69853aa3151927d427ca326c1b6aab8a03aaf55
Steven <stevj17@gmail.com>
2018-12-05 16:23:05 -0800

    FINAL UPDATES

--- app_SJ.py
+++ app_SJ.py
@@ -1,14 +1,13 @@
-from flask import Flask, jsonify, render_template, jsonify,request
+from flask import Flask, jsonify, render_template, jsonify
 from sklearn.externals import joblib
-import requests
+from flask import request
 import pandas as pd
 import numpy as np
 import pymongo
 import json
 import pickle
-
+import random
 from pymongo import MongoClient
-#from flask_pymongo import Pymongo
 
 model = joblib.load('models/danceability_model.pkl')
 
@@ -19,29 +18,161 @@ url = 'mongodb://admin:yaybootcamp2018@ds231228.mlab.com:31228/songs_ml'
 client = MongoClient(url)
 db = client['songs_ml']
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-	'''
-	data = requests.form.get('sendData')
-	X = pd.Dataframe(data) #
-	predictions = model.predict(X)
-	'''
-	return render_template("index.html")
 
-@app.route('/test', methods = ['POST'])
-def test():
-    
-    
-    
-    return flask.jsonify({'msg': 'success'})
-'''
-@app.route("/test", methods=['GET','POST'])
-def test():
-    clicked=None
-    if request.method == "POST":
-        clicked=request.json['data']
-    return render_template("test.html")
-'''
+@app.route("/")
+def index():
+	acousticness = request.args.get('acousticness')
+	energy = request.args.get('energy')
+	instrumentalness = request.args.get('instrumentalness')
+	liveness = request.args.get('liveness')
+	loudness = request.args.get('loudness')
+	tempo = request.args.get('tempo')
+	valence = request.args.get('valence')
+
+
+	# do something to the get request variables convert into same values as your 230, 13
+	data = [		
+		{'acousticness': acousticness},
+		{'energy': energy},
+		{'instrumentalness': instrumentalness},
+		{'liveness': liveness},
+		{'loudness': loudness},
+		{'tempo':  tempo},
+		{'valence': valence}
+	]
+
+	#print(data)
+	#print(type(data))
+
+	values_list = []
+	for features in data:
+		for k, v in features.items():
+			print('{}: {}'.format(k, v))
+			values_list.append(v)
+
+	print(values_list[0])
+	print(values_list[1])
+	print(values_list[2])
+	print(values_list[3])
+	print(values_list[4])
+	print(values_list[5])
+	print(values_list[6])
+	#print(type(values_list[0]))
+	
+
+	print("----------------------------")
+
+	print(type(values_list[0]))
+	print(type(values_list[1]))
+
+	print("----------------------------")
+	print("----------------------------")
+	print("FLOAT VALUE BELOW")
+
+	all_float_values = []
+
+	if type(values_list[0]) == str:
+		float_1 = float(values_list[0])
+		print(float_1)
+		print(type(float_1))
+		all_float_values.append(float_1)
+
+	if type(values_list[1]) == str:
+		float_2 = float(values_list[1])
+		print(float_2)
+		print(type(float_2))
+		all_float_values.append(float_2)
+
+	if type(values_list[2]) == str:
+		float_3 = float(values_list[2])
+		print(float_3)
+		print(type(float_3))
+		all_float_values.append(float_3)
+
+
+	if type(values_list[3]) == str:
+		float_4 = float(values_list[3])
+		print(float_4)
+		print(type(float_4))
+		all_float_values.append(float_4)
+
+	if type(values_list[4]) == str:
+		float_5 = float(values_list[4])
+		print(float_5)
+		print(type(float_5))
+		all_float_values.append(float_5)
+
+	if type(values_list[5]) == str:
+		float_6 = float(values_list[5])
+		print(float_6)
+		print(type(float_6))
+		all_float_values.append(float_6)
+
+	if type(values_list[6]) == str:
+		float_7 = float(values_list[6])
+		print(float_7)
+		print(type(float_7))
+		all_float_values.append(float_7)
+
+	print("------CONVERTED FLOATS INTO A NEW LIST-------")
+	print(all_float_values)
+	print("---------------------------------------------")
+	print("\n")
+
+	print("------ENUMERATION-------")
+	for counter, value in enumerate(all_float_values):
+		print(counter, value)
+	print("---------------------------------------------")
+	print("\n")
+
+	print("-----NUMPY ARRAY CONVERSION-------")
+
+	float_values_np = np.array(all_float_values)
+
+	final_reshaped_np = float_values_np.reshape(1, -1)
+
+	print(final_reshaped_np)
+	print(f"TYPE IS {type(final_reshaped_np)}")
+	print(f"SHAPE IS {final_reshaped_np.shape}")
+
+	print("\n")
+
+	print("---------------------------------------------")
+	print("------------ML MODEL RESULTS-----------------")
+
+	#-------------- MODEL ---------------------
+
+	# X = StandardScaler().fit_transform(X)
+	
+	if final_reshaped_np.shape == (1, 7):
+		predictions = model.predict(final_reshaped_np)
+		final_score = round(predictions[0], 3)
+		print("Calculating...")
+		print(f'Model prediction is {final_score}')
+
+
+		if final_score >= 0.75:
+			print("HIGH DANCEABILITY! :-D ")
+
+		if final_score >= 0.50 and final_score <= 0.75:
+			print("PRETTY DANCEABLE! :-)")
+
+		if final_score >= 0.25 and final_score <= 0.50:
+			print("Eh, decent danceability :-/")
+
+		if final_score < 0.25:
+			print("Awful... Can't dance to this :-(")
+
+
+	
+	# predictions = 3
+	
+	
+	# print(predictions)
+	# print(f'Model prediction is {predictions}')
+	# print(acousticness, '', energy, '', instrumentalness, '', liveness, '', loudness, '', tempo, '', valence) 
+	# return render_template("index.html", tits=predictions)
+	return render_template("index.html") #solution=final_score)
 
 
 @app.route("/data")
@@ -54,40 +185,23 @@ def data():
 	  v = list(i.values())[1]
 	  json_data.append(
 	 	{
-		 "track" : v[2],
-		 "acousticness" : v[1],
-		 "danceability" : v[0],
-		 "duration_ms" : v[3], 
-		 "energy" : v[4], 
-		 "instrumentalness" : v[5],
-		 "key" : v[6],
-		 "liveness": v[7],
-		 "loudness": v[8],
-		 "mode" : v[9], 
-		 "speechiness" : v[10],
-		 "tempo" : v[11], 
-		 "time_signature" : v[13], 
-		 "track" : v[14],
-		 "track_href" : v[15],
-		 "valence": v[16]
+		 "id" : v[0],
+		 "track" : v[1],
+		 "acousticness" : v[2],
+		 "danceability" : v[3],
+		 "duration_ms" : v[4], 
+		 "energy" : v[5],
+		 "instrumentalness" : v[6],
+		 "key" : v[7],
+		 "liveness": v[8],
+		 "loudness": v[9],
+		 "mode" : v[10], 
+		 "speechiness" : v[11],
+		 "tempo" : v[12], 
+		 "valence": v[13]
 		})
 	#print(json_data)
 	return jsonify(json_data)
-# #load model
-# model = pickle.load(open('decision_tree_model.pkl', 'r'))
-
-# #define route with post request
-# @app.route('/', methods=["POST"])
-# def index():
-# 	# get array of features from post requests
-# 	# features = request.get_json()
-
-# 	# #creating a response object
-#  #    #storing the model's prediction in the object
-# 	# response = {}
-# 	# response['predictions'] = model.predict([features]).tolist()
-
-# 	# return jsonify(response)
 
 if __name__ == '__main__':
 	app.run(debug=True)
